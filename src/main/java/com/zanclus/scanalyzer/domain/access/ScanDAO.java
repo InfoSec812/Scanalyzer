c9fd9564e5e65591127455e5f8971af927a88eaa
Deven Phillips <deven.phillips@gmail.com>
2014-02-10 20:44:18 -0500

    More JavaDoc updates.

--- src/main/java/com/zanclus/scanalyzer/domain/access/ScanDAO.java
+++ src/main/java/com/zanclus/scanalyzer/domain/access/ScanDAO.java
@@ -4,6 +4,8 @@
 package com.zanclus.scanalyzer.domain.access;
 
 import java.util.List;
+
+import com.zanclus.scanalyzer.domain.entities.Host;
 import com.zanclus.scanalyzer.domain.entities.Scan;
 
 /**
@@ -14,6 +16,13 @@ import com.zanclus.scanalyzer.domain.entities.Scan;
  */
 public class ScanDAO extends GenericDAO<Scan, Long> {
 
+	/**
+	 * Get the paginated scans history associated with a given hostId
+	 * @param hostId The id which identifies the {@link Host}
+	 * @param limit 
+	 * @param offset
+	 * @return
+	 */
 	public List<Scan> getPagedScansByHostId(Long hostId, int limit, int offset) {
 		em.getTransaction().begin() ;
 		List<Scan> retVal = em.createQuery("FROM Scan s WHERE s.target.id=:hostId", Scan.class)
